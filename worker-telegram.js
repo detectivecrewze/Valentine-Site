6f146eb5c8e3de76ffce9c151327aa24a5b76469
detectivecrewze <aldoramadhan16@gmail.com>
2026-02-05 00:27:48 +0700

    Add Pragma to CORS allowed headers

--- worker-telegram.js
+++ worker-telegram.js
@@ -4,7 +4,7 @@ var index_default = {
         const corsHeaders = {
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-            "Access-Control-Allow-Headers": "Content-Type, Cache-Control"
+            "Access-Control-Allow-Headers": "Content-Type, Cache-Control, Pragma"
         };
 
         // Handle CORS Preflight
